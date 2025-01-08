fcb64fcc4743c1e43ccebdc6bae6b7b477e127e6
Erkan KARADAĞ <109920900+erkankaradag@users.noreply.github.com>
2025-01-08 14:17:25 +0300

    Update Person.cs

--- OkulYonetimi/Person.cs
+++ OkulYonetimi/Person.cs
@@ -11,33 +11,5 @@ namespace SchoolBoard
         public string Name { get; set; } // Public özellik
         public string Surname { get; set; } // Public özellik
         public DateTime DateBirth { get; set; } // Public özellik
-            }
-            set
-            {
-                Name = value;
-            }
-        }
-        private string Surname
-        {
-            get
-            {
-                return Surname;
-            }
-            set
-            {
-                Surname = value;
-            }
-        }
-        private DateTime DateBirth
-        {
-            get
-            {
-                return DateBirth;
-            }
-            set
-            {
-                DateBirth = value;
-            }
-        }
     }
 }
